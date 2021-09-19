c4d95aa193427ad1ddb799105100c70c46a08b43
William Chen <wtchen@Williams-Mac-mini.local>
2021-09-18 23:04:30 -0700

    add basic security - 2

--- elexr/routers/elections.py
+++ elexr/routers/elections.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 from voting import apportionment
 from ..types import election, message
+from ..security import oauth2_scheme
 
 router = APIRouter(
     prefix="/elections",
@@ -10,7 +11,7 @@ router = APIRouter(
 
 
 @router.post("/method/{method_name}", responses={405: {"model": message.Message}})
-def election(method_name: str, election_info: election.ElectionInfo):
+def election(method_name: str, election_info: election.ElectionInfo, token: str = Depends(oauth2_scheme)):
     valid_methods = {"dhondt": apportionment.dhondt, "sainte_lague": apportionment.sainte_lague}
     if method_name not in valid_methods:
         raise HTTPException(status_code=405, detail={"message": "Invalid apportionment method"})
@@ -18,5 +19,6 @@ def election(method_name: str, election_info: election.ElectionInfo):
     seats = election_info.seats
     assignments = valid_methods[method_name](votes, seats)
     return {
-        "seats": [{"party": result.party, "seats": assignments[i]} for i, result in enumerate(election_info.results)]
+        "seats": [{"party": result.party, "seats": assignments[i]} for i, result in enumerate(election_info.results)],
+        "token": token
     }

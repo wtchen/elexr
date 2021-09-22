b9c7369f86db8feef2c2bd510a3e1fc93d08f303
William Chen <wtchen@Williams-Mac-mini.local>
2021-09-21 22:16:01 -0700

    add SQL and Docker support

--- elexr/main.py
+++ elexr/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, HTTPException, Depends
 from .routers import elections, users
+from fastapi.security import OAuth2PasswordRequestForm
+from .db import database, User
 
 app = FastAPI()
 
@@ -8,15 +9,28 @@ app.include_router(elections.router)
 app.include_router(users.router)
 
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/")
 async def read_root():
-    return """
-        <html>
-            <head>
-                <title>Elexr</title>
-            </head>
-            <body>
-                <h1>Elexr</h1>
-            </body>
-        </html>
-        """
+    return await User.objects.all()
+
+
+@app.on_event("startup")
+async def startup():
+    if not database.is_connected:
+        await database.connect()
+    # create a dummy entry
+    await User.objects.get_or_create(email="test@test.com")
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    if database.is_connected:
+        await database.disconnect()
+
+
+@app.post("/token")
+def token(form_data: OAuth2PasswordRequestForm = Depends()):
+    if form_data.username != "william" or form_data.password != "password":
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": "william", "token_type": "bearer"}
